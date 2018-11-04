92dbede1590be00e1179577db4871bce12647169
Sergio Rodríguez Marín <sergio_basket93@hotmail.com>
2018-11-04 22:45:45 +0100

    Cambiando respuesta a JSON

--- src/app.js
+++ src/app.js
@@ -5,7 +5,8 @@ const teacher     = require('./teacher.js');
 const app = express();
 
 app.get('/', (req, res) => {
-   res.send('{"status":"OK"}');
+   res.setHeader('Content-Type','application/json');
+   res.send(JSON.stringify({'status':'ok'}));
 })
 
 app.get('/student/all', (req,res) => {
