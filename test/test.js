8e4079c1f8ed928c0423c72780687c303db54d1d
Sergio Rodríguez Marín <sergio_basket93@hotmail.com>
2018-11-07 00:00:26 +0100

    Cambio en los tests

--- test/test.js
+++ test/test.js
@@ -11,13 +11,13 @@ describe('Estudiante', function(){
 
     describe('Crear usuario', function(){
         it('Devuelve un string como nombre del estudiante', function(){
-            var student = new studentClass.Student('Gregorio', 14);
+            var student = new studentClass('Gregorio', 14,[]);
             expect(student.getName()).to.be("Gregorio");
         });
     });
     describe('Consultar edad', function(){
         it('Devuelve la edad del estudiante', function(){
-            var student = new studentClass.Student('Gregorio', 14);
+            var student = new studentClass('Gregorio', 14,[]);
             expect(student.getAge()).to.be(14);
         });
     });
